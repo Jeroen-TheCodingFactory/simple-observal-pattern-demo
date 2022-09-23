e6a4f119d1a0cb722cd14f3a12bc2bf3793f2d6a
Jeroen Rijsdijk <j.rijsdijk@ma-web.nl>
2022-09-23 22:01:21 +0200

    Made a simple observable pattern that renders to html/view

--- js/main.js
+++ js/main.js
@@ -26,28 +26,42 @@ class Subscriber {
 }
 
 class Observer {
-    constructor(name, subscriber = null) {
+    constructor(name, subscriber = null, htmlElementById) {
         this.name = name;
         this.subscriber = subscriber;
         this.subscriber.attach(this);
+        this.htmlElementById = htmlElementById;
         console.log(this.name + " original value is: " + this.subscriber.getState());
     }
     update(){
-        console.log(this.name + " the value now is: " + this.subscriber.getState());
+            this.render(this.subscriber.getState());
+    }    
+    render(value){
+        this.htmlElementById.innerText = value;
     }
-    
 }
 
 let number = 0;
 let subscriber = new Subscriber(number);
+let observer = new Observer("Observer 1",subscriber,document.getElementById("js--h2--1"));
+let observer2 = new Observer("Observer 2",subscriber,document.getElementById("js--h2--2"));
+let observer3 = new Observer("Observer 3",subscriber,document.getElementById("js--h2--3"));
 
-for(let i = 1; i <= 1000; i++){
-    let observer = new Observer(i,subscriber);
-}
+let number2 = 10;
+let subscriber2 = new Subscriber(number2);
+let observer4 = new Observer("Observer 4",subscriber2,document.getElementById("js--h2--4"));
+let observer5 = new Observer("Observer 5",subscriber2,document.getElementById("js--h2--5"));
+let observer6 = new Observer("Observer 6",subscriber2,document.getElementById("js--h2--6"));
+
 
 setInterval( () => {
     number += 1;
     subscriber.setState(number);
+}, 1000)
+
+setInterval( () => {
+    number2 -= 1;
+    subscriber2.setState(number2);
 }, 1000)
 
 
